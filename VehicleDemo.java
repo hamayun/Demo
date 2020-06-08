b9399f8e599a6de4dcebb25143bd77725040f7b3
Mian M. Hamayun <m.m.hamayun@bham.ac.uk>
2020-06-08 13:47:57 +0100

    Add Bike class and .gitignore file
    Signed-off-by: Mian M. Hamayun <m.m.hamayun@bham.ac.uk>

--- VehicleDemo.java
+++ VehicleDemo.java
@@ -34,6 +34,35 @@ class Bicycle implements Vehicle{
     } 
 } 
 
+class Bike implements Vehicle {
+    int speed;
+    int gear;
+
+    // to change gear
+    @Override
+    public void changeGear(int newGear){
+        gear = newGear;
+    }
+
+    // to increase speed
+    @Override
+    public void speedUp(int increment){
+        speed = speed + increment;
+    }
+
+    // to decrease speed
+    @Override
+    public void applyBrakes(int decrement){
+        speed = speed - decrement;
+    }
+
+    public void printStates() {
+         System.out.println("speed: " + speed
+             + " gear: " + gear);
+    }
+
+}
+
 public class VehicleDemo { 
     public static void main (String[] args) { 
         // creating an inatance of Bicycle  
@@ -45,6 +74,15 @@ public class VehicleDemo {
         
         System.out.println("Bicycle present state :"); 
         bicycle.printStates();
+
+        // creating instance of bike.
+        Bike bike = new Bike();
+        bike.changeGear(1);
+        bike.speedUp(4);
+        bike.applyBrakes(3);
+
+        System.out.println("Bike present state :");
+        bike.printStates();
     } 
 } 
 
