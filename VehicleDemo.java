b0f71282dbfa630a20e918cf5b0edc155aa4ece2
Mian M. Hamayun <m.m.hamayun@bham.ac.uk>
2020-06-08 14:48:51 +0100

    Add Implementation of Car class
    Signed-off-by: Mian M. Hamayun <m.m.hamayun@bham.ac.uk>

--- VehicleDemo.java
+++ VehicleDemo.java
@@ -63,6 +63,38 @@ class Bike implements Vehicle {
 
 }
 
+class Car implements Vehicle{
+    int speed;
+    int gear;
+    String make_model;
+
+    public Car(String mm){
+	make_model = mm;
+    }
+
+    // to change gear
+    @Override
+    public void changeGear(int newGear){
+	gear = newGear;
+    }
+
+    // to increase speed
+    @Override
+    public void speedUp(int increment){
+	speed = speed + (increment * 2);
+    }
+
+    // to decrease speed
+    @Override
+    public void applyBrakes(int decrement){
+	speed = speed - (decrement * 2);
+    }
+
+    public void printStates() {
+         System.out.println(make_model + " speed: " + speed + " gear: " + gear);
+    }
+}
+
 public class VehicleDemo { 
     public static void main (String[] args) { 
         // creating an inatance of Bicycle  
@@ -83,6 +115,15 @@ public class VehicleDemo {
 
         System.out.println("Bike present state :");
         bike.printStates();
+
+        // creating instance of Car.
+        Car car = new Car("Honda-Civic");
+        car.changeGear(2);
+        car.speedUp(4);
+        car.applyBrakes(1);
+
+        System.out.println("Car present state :");
+        car.printStates();
     } 
 } 
 
